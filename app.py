dc08026a9cd27fe119599b04ec71aaed6e83f948
SakthiSairam123 <sec23ec145@sairamtap.edu.in>
2026-06-11 15:12:49 +0530

    testing

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ def get_rag_chain():
 
         chatModel = ChatGroq(
             model="llama-3.3-70b-versatile",
-            api_key=GROQ_API_KEY,
+            api_key=OPENAI_API_KEY,
             temperature=0.2
         )
 
